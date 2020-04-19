4fc676c0e67c82b59780bdcea9330776c5144b7d
Andrey <am8296235@mail.com>
2020-04-19 23:19:22 +0300

    commit

---
+++ catalog/js/script.js
@@ -0,0 +1,93 @@
+const catalogProduct = [
+    {
+        id: 'id1',
+        name: 'Product1',
+        img: './image-catalog/catalog1.png',
+        price: '$20.00'  
+    },
+    {
+        id: 'id2',
+        name: 'Product2',
+        img: './image-catalog/catalog2.png',
+        price: '$42.00'  
+    },
+    {
+        id: 'id3',
+        name: 'Product3',
+        img: './image-catalog/catalog3.png',
+        price: '$33.00'  
+    },
+    {
+        id: 'id4',
+        name: 'Product4',
+        img: './image-catalog/catalog4.png',
+        price: '$54.00'
+    },
+    {
+        id: 'id5',
+        name: 'Product5',
+        img: './image-catalog/catalog5.png',
+        price: '$56.00'  
+    }
+    ,
+    {
+        id: 'id6',
+        name: 'Product6',
+        img: './image-catalog/catalog6.png',
+        price: '$21.00'  
+    },
+    {
+        id: 'id7',
+        name: 'Product7',
+        img: './image-catalog/catalog7.png',
+        price: '$74.00'  
+    },
+    {
+        id: 'id8',
+        name: 'Product8',
+        img: './image-catalog/catalog8.png',
+        price: '$63.00' 
+    },
+    {
+        id: 'id9',
+        name: 'Product9',
+        img: './image-catalog/catalog9.png',
+        price: '$57.00'  
+    },
+    {
+        id: 'id10',
+        name: 'Product10',
+        img: './image-catalog/catalog10.png',
+        price: '$48.00'  
+    },
+    {
+        id: 'id11',
+        name: 'Product11',
+        img: './image-catalog/catalog11.png',
+        price: '$51.00'  
+    },
+    {
+        id: 'id12',
+        name: 'Product12',
+        img: './image-catalog/catalog12.png',
+        price: '$29.00'  
+    },
+    {
+        id: 'id13',
+        name: 'Product13',
+        img: './image-catalog/catalog11.png',
+        price: '$61.00'  
+    },
+    {
+        id: 'id14',
+        name: 'Product14',
+        img: './image-catalog/catalog12.png',
+        price: '$43.00'  
+    },
+    {
+        id: 'id15',
+        name: 'Product15',
+        img: './image-catalog/catalog12.png',
+        price: '$78.00' 
+    }
+]
